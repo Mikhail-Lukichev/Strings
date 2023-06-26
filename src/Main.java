46cd89508441ede6f1c229926fc089536ef6697b
Mikhail Lukichev <dextrumm@gmail.com>
2023-06-26 16:58:38 -0600

    Strings Homework 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-
+        task1();
+        task2();
+        task3();
     }
+    public static void task1() {
+        System.out.println("Task1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+    public static void task2() {
+        System.out.println("Task2");
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    }
+    public static void task3() {
+        System.out.println("Task3");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё','e');
+        fullName = fullName.replace('Ё','Е');
+        System.out.println("Данные ФИО сотрудника —" + fullName);
+    }
+
 }
